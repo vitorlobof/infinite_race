3ee61a401853b1a8c987cd81ea7742bea0236f5f
Vítor Lobo <vitorlobof@gmail.com>
2024-03-17 19:40:42 -0300

    feat: Rewrites the main.

---
+++ include/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include "display.h"
+#include "buzzer.h"
+#include "../lib/objects/objects.h"
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int obstaclesCount = 0;
+const int obstaclesMaxCount = 5;
+Obstacle obstacles[obstaclesMaxCount];
+Vector obstaclesDisplacement(0, -3);
+
+Circle player = createPlayer();
+
+#endif
